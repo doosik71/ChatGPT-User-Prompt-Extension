5c6ae21dc8e707f527191f157ebf5fd18aad990e
Doosik Kim <doosik@etri.re.kr>
2023-10-24 16:41:07 +0900

    First commit.

---
+++ README.md
@@ -0,0 +1,4 @@
+# ChatGPT Text Summary Extension
+
+- Press [F9] to summarize text from clipboard.
+- Install at <edge://extensions/>
---
+++ scripts/content.js
@@ -0,0 +1,18 @@
+document.addEventListener("keydown", async function (event) {
+    if (event.key === "F9" || event.code === "F9") {
+        const clipboardText = await navigator.clipboard.readText();
+        if (clipboardText) {
+            const textarea = document.querySelector("#prompt-textarea");
+            if (textarea) {
+                textarea.value = "Summarize:\n" + clipboardText;
+                var event = new Event("input", { bubbles: true, cancelable: true });
+                textarea.dispatchEvent(event);
+
+                const button = document.querySelector("form.stretch button.absolute");
+                if (button) {
+                    button.click();
+                }
+            }
+        }
+    }
+})
