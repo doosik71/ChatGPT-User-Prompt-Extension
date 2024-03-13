2f1debcd40a149520f35bd3643133068a912ddb7
Doosik Kim <doosik@etri.re.kr>
2024-03-13 17:44:34 +0900

    Add script.

--- scripts/content.js
+++ scripts/content.js
@@ -2,6 +2,7 @@ const text_summary_options = [
     `Please summarize the key points in complete sentences using Markdown bullet style:\n{{text}}`,
     `Please summarize the key points as concisely as possible using Markdown bullet style:\n{{text}}`,
     `Please explain the following in detail:\n{{text}}`,
+    `Say the following sentences in descriptive form:\n{{text}}`,
     `Correct the following sentence:\n{{text}}`,
     `Translate into English:\n{{text}}`,
     `Translate into Korean:\n{{text}}`,
